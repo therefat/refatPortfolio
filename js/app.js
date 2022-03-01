407909c5746bee8e3c5acf0e52335344858c0791
mdrefathossen <refathossen2001@gmail.com>
2022-03-02 00:36:42 +0600

    project 4th semister

---
+++ js/app.js
@@ -0,0 +1,43 @@
+//navbar scrolling
+window.onscroll = function() {scrollFunction()};
+
+
+const humburger = document.querySelector(".navbar-toggler");
+const mainNav = document.getElementById("mainNav")
+
+function scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        mainNav.classList.add('navbar-reduce');
+    } else {
+        mainNav.classList.remove('navbar-reduce');
+    }
+  }
+
+// navbar button
+humburger.addEventListener("click", function(){
+        if(!mainNav.classList.contains('navbar-reduce')){
+            mainNav.classList.add('navbar-reduce');
+        } else{
+            mainNav.classList.remove('navbar-reduce');
+        }
+});
+
+// typing text animation script
+ 
+
+var typed = new Typed('.typing',{
+    strings:['Youtuber', "Developer", 'Blogger', "Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+});
+var typed = new Typed('.typing-2',{
+    strings:['Youtuber', "Developer", 'Blogger', "Designer"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+
+});
+
+ 
+
